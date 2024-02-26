91c940f1c17141ff7956a70a864ab48573226ddf
Sanne Wielinga <sannwielinga@gmail.com>
2024-02-26 16:10:51 +0100

    Trying out OpenAI Gymnasium

---
+++ single_agents.py
@@ -0,0 +1,29 @@
+import gymnasium as gym
+
+env = gym.make('CartPole-v1', render_mode="human")
+observation, info = env.reset()
+
+for episode in range(20):  # Run 20 episodes
+    observation = env.reset() 
+    for _ in range(1000):
+        action = env.action_space.sample()  # agent policy that uses the observation and info
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+env.close()
+
+env = gym.make('MountainCar-v0', render_mode="human")
+observation, info = env.reset()
+
+for episode in range(20):  # Run 20 episodes
+    observation = env.reset()  # Reset the environment to start a new episode
+    for _ in range(1000):
+        action = env.action_space.sample()  # agent policy that uses the observation and info
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+env.close()
